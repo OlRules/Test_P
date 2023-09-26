25a7d628fd6d405b610e5a84120c51ff3f3afcea
OlRules <vm92@yandex.ru>
2023-09-26 15:15:36 +0500

    add test_delete? status

---
+++ data/status_code.py
@@ -0,0 +1,7 @@
+
+
+class StatusCode:
+    CREATE = 201
+    OK = 200
+    BAD_FOUND = 400
+    NOT_FOUND = 404
--- test/test_create_users.py
+++ test/test_create_users.py
@@ -1,20 +1,37 @@
 from src.my_requests import MyRequests
-
+from generator.generator import generated_person
+from src.assertions import Assertion
+from data.status_code import StatusCode
 
 class TestCreateUsers:
-    body = {
-    'first_name': 'Denis',
-    'last_name': 'Denis2',
-    'company_id': 1
-    }
+    assertions = Assertion()
+    status_code = StatusCode()
 
+    def get_body(self,first_name, last_name, company_id):
+        body = {
+            'first_name': first_name,
+            'last_name': last_name,
+            'company_id': company_id
+        }
+        return body
     def test_create_user(self):
-        response = MyRequests.post('/users/', self.body)
-        assert response.json()['first_name']  == self.body.get('first_name'), "First name was not created"
-        assert response.json()['last_name'] == self.body.get('last_name'), "last name was not created"
-        print(response.json())
+        person_info = next(generated_person())
+        first_name = person_info.first_name
+        last_name = person_info.last_name
+        company_id = person_info.company_id
+        response = MyRequests.post('/users/', self.get_body(first_name, last_name, company_id))
+        body = response.json()
+        print(body)
+        assert body['first_name']  == first_name, "First name was not created"
+        assert body['last_name'] == last_name, "last name was not created"
+
     def test_get_status_code_201(self):
-        response = MyRequests.post('/users/', self.body)
-        assert response.status_code  == 201, f"Status code not 201, status code is {response.status_code}"
+        person_info = next(generated_person())
+        first_name = person_info.first_name
+        last_name = person_info.last_name
+        company_id = person_info.company_id
+        response = MyRequests.post('/users/', self.get_body(first_name, last_name, company_id))
+        self.assertions.assert_status_code(response, self.status_code.CREATE)
+        # assert response.status_code  == 201, f"Status code not 201, status code is {response.status_code}"
 
 
